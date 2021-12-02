8521c9a981d06171df8c32ca730dd24496c0359d
Prince Bansal <1726250.cse.coe@cgc.edu.in>
2021-12-02 21:18:01 +0530

    Home route added

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Hey");
+});
+
 app.use("/todo", require("./routes/TodoRoutes"));
 
 app.listen(PORT, () => console.log("App Running"));
